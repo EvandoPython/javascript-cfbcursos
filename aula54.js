1d4b07ab18026e4c61394a974996bba8880905d2
Evando Silva <evando_5@hotmail.com>
2023-07-01 15:25:05 -0300

    att

---
+++ aula54.js
@@ -0,0 +1,10 @@
+const btn_soma=document.querySelector("#caixa")
+
+let mapa = new Map()
+
+mapa.set("curso","JavaScriptp")
+mapa.set(10,"Banco de Dados")
+
+console.log(mapa)
+
+caixa.innerHTML=mapa.get("curso")
